4b3430e4c583c5ecd91764b3bee03604509c9089
Kurt Peek <kurtpeek@Kurts-MacBook-Pro.local>
2018-08-26 11:14:59 -0700

    [WIP] Attempt at adapting the Google Maps Custom Overlay example to react-google-maps

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import MapWithAnOverlayView from './components/overlayView'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Custom Overlay</h1>
+        </header>
+        <MapWithAnOverlayView
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBimnrhiugaGSNN8WnsjpzMNJcrH_T60GI&v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `800px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/overlayView.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+const { compose, withProps, withStateHandlers } = require("recompose");
+const {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  OverlayView,
+} = require("react-google-maps");
+
+
+class CustomOverlayView extends OverlayView {
+  onAdd() {
+    this.containerElement = document.createElement('div');
+    this.containerElement.style.position = 'absolute';
+    this.containerElement.style.borderStyle = 'none';
+    this.containerElement.style.borderWidth = '0px';
+
+    // Create the img element and attach it to the div.
+    var img = document.createElement('img');
+    img.src = this.props.image;
+    img.style.width = '100%';
+    img.style.height = '100%';
+    img.style.position = 'absolute';
+    this.containerElement.appendChild(img);
+  }
+}
+
+// const getPixelPositionOffset = (width, height) => ({
+//   x: -(width / 2),
+//   y: -(height / 2),
+// })
+
+const MapWithAnOverlayView = compose(
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: 62.323907, lng: -150.109291 }}
+  >
+    <CustomOverlayView
+      // position={{ lat: -34.397, lng: 150.644 }}
+
+      // Image from https://developers.google.com/maps/documentation/javascript/examples/full/images/talkeetna.png
+      image = { require("../img/talkeetna.png") }
+
+      /*
+       * An alternative to specifying position is specifying bounds.
+       * bounds can either be an instance of google.maps.LatLngBounds
+       * or an object in the following format:
+       */
+      bounds={{
+        ne: { lat: 62.400471, lng: -150.005608 },
+        sw: { lat: 62.281819, lng: -150.287132 }
+      }}
+
+      /*
+       * 1. Specify the pane the OverlayView will be rendered to. For
+       *    mouse interactivity, use `OverlayView.OVERLAY_MOUSE_TARGET`.
+       *    Defaults to `OverlayView.OVERLAY_LAYER`.
+       */
+      mapPaneName={OverlayView.OVERLAY_LAYER}
+      /*
+       * 2. Tweak the OverlayView's pixel position. In this case, we're
+       *    centering the content.
+       */
+      // getPixelPositionOffset={getPixelPositionOffset}
+      /*
+       * 3. Create OverlayView content using standard React components.
+       */
+    >
+      <img src={require("../img/talkeetna.png")} alt="overlay"/>
+    </CustomOverlayView>
+  </GoogleMap>
+);
+
+export default MapWithAnOverlayView;
