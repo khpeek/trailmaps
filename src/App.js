706fc9acf97ed95895c3dd0ea26a8c538e971e0a
Kurt Peek <kurtpeek@Kurts-MacBook-Pro.local>
2018-09-01 19:17:34 -0700

    [WIP] Attempt to display groundOverlay component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import MapWithAnOverlayView from './components/overlayView'
+import MapWithAnOverlayView from './components/overlayView';
+import MapWithGroundOverlay from './components/groundOverlay';
 
 class App extends Component {
   render() {
@@ -9,10 +10,10 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Custom Overlay</h1>
         </header>
-        <MapWithAnOverlayView
+        <MapWithGroundOverlay
           googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBimnrhiugaGSNN8WnsjpzMNJcrH_T60GI&v=3.exp&libraries=geometry,drawing,places"
           loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `800px` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
           mapElement={<div style={{ height: `100%` }} />}
         />
       </div>
---
+++ src/components/groundOverlay.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const { compose } = require("recompose");
+const {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  GroundOverlay,
+} = require("react-google-maps");
+
+const MapWithGroundOverlay = compose(
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    defaultZoom={12}
+    defaultCenter={{lat: 40.740, lng: -74.18}}
+  >
+    <GroundOverlay
+      defaultUrl="https://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg"
+      defaultBounds={new google.maps.LatLngBounds(
+        new google.maps.LatLng(40.712216, -74.22655),
+        new google.maps.LatLng(40.773941, -74.12544)
+      )}
+      defaultOpacity={.5}
+    />
+  </GoogleMap>
+);
+
+<MapWithGroundOverlay
+  googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBimnrhiugaGSNN8WnsjpzMNJcrH_T60GI&v=3.exp&libraries=geometry,drawing,places"
+  loadingElement={<div style={{ height: `100%` }} />}
+  containerElement={<div style={{ height: `400px` }} />}
+  mapElement={<div style={{ height: `100%` }} />}
+/>
